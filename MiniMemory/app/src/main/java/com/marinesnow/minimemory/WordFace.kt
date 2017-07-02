a25f7a998b3358e8b71ceb2228d80b69b5ac1461
misaki-mei <2112736437@qq.com>
2017-07-02 12:48:19 +0800

    alert WordFace

--- MiniMemory/app/src/main/java/com/marinesnow/minimemory/WordFace.kt
+++ MiniMemory/app/src/main/java/com/marinesnow/minimemory/WordFace.kt
@@ -2,18 +2,31 @@ package com.marinesnow.minimemory
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+import android.os.Message
 import kotlinx.android.synthetic.main.word_face.*
 import org.jetbrains.anko.toast
 
 
 class WordFace : AppCompatActivity() {
 
+    private val mHandler by lazy { object : Handler(){
+        override fun handleMessage(msg: Message?) {
+            when(msg?.what){
+                0 -> printer.setText(current)
+            }
+            super.handleMessage(msg)
+        }
+    } }
+
     var cur = 0
+    var current = ""
     lateinit var resList : ArrayList<WordBean>
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.word_face)
         extractRes()
+        afterClick()
     }
 
     fun extractRes(){
@@ -26,8 +39,31 @@ class WordFace : AppCompatActivity() {
     }
 
     fun initUI(){
-        right_btn.setOnClickListener { resList[cur].add(3) }
-        wrong_btn.setOnClickListener { resList[cur].markWrong() }
-        puzzle_btn.setOnClickListener { resList[cur].markPuzzle() }
+        right_btn.setOnClickListener {
+            if(!resList[cur].add(3)){
+                resList.removeAt(cur)
+            }else cur++
+            afterClick()
+        }
+        wrong_btn.setOnClickListener {
+            resList[cur].markWrong()
+            cur++
+            afterClick()
+        }
+        puzzle_btn.setOnClickListener {
+            if(!resList[cur].markPuzzle()){
+                resList.removeAt(cur)
+            }else cur++
+            afterClick()
+        }
+        printer.setOnClickListener {
+            
+        }
+    }
+
+    fun afterClick(){
+        cur = cur%resList.size
+        current = resList[cur].word
+        mHandler.sendEmptyMessage(0)
     }
 }
